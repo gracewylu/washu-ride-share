28e720c31b70e074d0d17c4c940a85b8ded9d883
Samuel Teeter <samteeter93@yahoo.com>
2016-03-02 21:11:33 -0600

    Updated new trip script so that it also inserts into trips2users.

--- home.php
+++ home.php
@@ -10,7 +10,7 @@
 ?>
 <html>
     <head>
-        <title>Get a lift</title>
+        <title>Get A Ride</title>
         <!--Materialize setup-->
       <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
