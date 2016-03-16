85060e14550683785e3f90fa6b2114b3f8f99575
Samuel Teeter <samteeter93@yahoo.com>
2016-03-15 19:05:55 -0500

    Fixed materialize styling on new trip page.

--- create_trip.php
+++ create_trip.php
@@ -22,85 +22,69 @@ document.addEventListener('DOMContentLoaded', function() {
     include('navbar.php');
 ?>
 
-<div class="row">
-  <form class="col s12" action="new_trip.php" method="POST">
-    <div class="row">
-      <div class="input-field col s6">
-        <input id="drop_off_location" name="drop_off_location" type="text" class="validate">
-        <label for="drop_off_location">Event</label>
-    <!-- Event: <input type="text" name="drop_off_location">
-  <br> -->
-      </div>
-    </div>
-    <div class="row">
-        <div class="input-field col s6">
-        <input id="date" name="date">
-        <label for="date">Date</label>
-      </div>
-    </div>
-    <div class="row">
-        <div class="input-field col s6">
-        <input id="pick_up_location" name="pick_up_location" type="text" class="validate">
-        <label for="pick_up_location">Pick-up Spot</label>
-      </div>
-    </div>
-    <div class="row">
-        <div class="input-field col s6">
-        <input id="drop_off_location" name="drop_off_location" type="text" class="validate">
-        <label for="drop_off_location">Drop-off Location</label>
-      </div>
-    </div>
-    <div class="row">
-        <div class="input-field col s6">
-        <select>
-          <option value="1">Car 1</option>
-          <option value="2">Car 2</option>
-        </select>
-        <label for="car">Car</label>
-      </div>
-    </div>
-    <div class="row">
-        <div class="input-field col s6">
-        <input id="seats_available" name="seats_available" type="text" class="validate" value="1" min="1" max="5">
-        <label for="seats_available">Available Seats</label>
-      </div>
-    </div>
-  
-  
- <!--  Date: 
-  <input type="date" id="date" name="date">
-  <br>
-  
-  Pick-up Spot:
-  <input type="text" name="pick_up_location">
-  <br>
-  
-  Drop-off Location:
-  <input type="text" name="drop_off_location">
-  <br>
-  <!--get car id from car-->
-  <!-- Car:
-  <select name="car">
-  <option value="Car1">Car1</option>
-  <option value="Car2">Car2</option>
-  </select>
-  <br> -->
-  
-  <!-- Available Seats: -->
-  <!--change 'max' to max number of seats in car-->
-  <!-- should be updated when user changes car-->
-<!--   <input type="number" name="seats_available" value="1" min="1" max="5"> --> 
-  <input type="hidden" name="username" value="%d">
- 
- <div class="row"> 
-    <div class="col l4">
-      <input type="submit" value="Submit" class="btn waves-effect waves-light custom-button blue-background">
-    </div>
-</div>
-  </select>
-</form>
-</div>
+	<div class="row">
+		<form class="col s12" action="new_trip.php" method="POST">
+		  <div class="row">
+			  <div class="input-field col s6">
+			  <input type="date" class="datepicker" id="date" name="date">
+			  <label for="date">Date</label>
+			</div>
+		  </div>
+		  <div class="row">
+			  <div class="input-field col s6">
+			  <input id="pick_up_location" name="pick_up_location" type="text" class="validate"/>
+			  <label for="pick_up_location">Pick-up Spot</label>
+			</div>
+		  </div>
+		  <div class="row">
+			  <div class="input-field col s6">
+			  <input id="drop_off_location" name="drop_off_location" type="text" class="validate">
+			  <label for="drop_off_location">Drop-off Location</label>
+			</div>
+		  </div>
+		  <div class="row">
+			  <div class="input-field col s6">
+			  <select>
+				<option value="1">Car 1</option>
+				<option value="2">Car 2</option>
+			  </select>
+			  <label for="car">Car</label>
+			</div>
+		  </div>
+		  <div class="row">
+			  <div class="input-field col s6">
+			  <input id="seats_available" name="seats_available" type="text" class="validate" value="1" min="1" max="5">
+			  <label for="seats_available">Available Seats</label>
+			</div>
+		  </div>
+	   
+		<input type="hidden" name="username" value="%d">
+	   
+		  <div class="row"> 
+			  <div class="col l4">
+				<input type="submit" value="Submit" class="btn waves-effect waves-light custom-button blue-background">
+			  </div>
+		  </div>
+	  </form>
+	</div>
 
 </body>
+
+<!--Finish Materialize setup-->
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+        
+      <script>
+		//Set document ready function so mobile navbar button works
+        $( document ).ready(function() {
+            $(".button-collapse").sideNav();
+            $('.modal-trigger').leanModal();
+        });
+		
+		//Set up for datepickers
+		$('.datepicker').pickadate({
+			selectMonths: true, // Creates a dropdown to control month
+		  });
+	  </script>
 
 </html>
