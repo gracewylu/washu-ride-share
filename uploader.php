e57fcb9cde0139e049c6299ead3faab6bac7157e
Kevin Tsao <kevintsao@davids-mbp.dhcp.wustl.edu>
2016-04-26 18:41:20 -0500

    user_profile fixed empty piture formatting

--- uploader.php
+++ uploader.php
@@ -16,15 +16,20 @@ if( !preg_match('/^[\w_\-]+$/', $username) ){
 	echo "Invalid username";
 	exit;
 }
+$path = sprintf("/home/cse437/user_pictures/%s",$username);
+if(file_exists($path)){
+$files = scandir($path);
+foreach($files as $file){
+	if(is_file($path.'/'.$file)){
+		unlink($path.'/'.$file);
+	}
+}
+}
+
 if(!file_exists(sprintf("/home/cse437/user_pictures/%s",$username))){
 	mkdir(sprintf("/home/cse437/user_pictures/%s",$username),0777,true);
 }
-$path = sprintf("/home/cse437/user_pictures/%s",$username);
-$files = scandir($path);
-$firstFile = $files[2];
-if($firstFile){
-	unlink($path . $firstFile);
-}
+
 $full_path = sprintf("/home/cse437/user_pictures/%s/%s", $username, $filename);
 
 if($_FILES['uploadfile']['error']==0){ 
